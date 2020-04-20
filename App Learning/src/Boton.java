d40db6e7802f3c46c503cabf652945a5209f981a
Maximiliano Castro <maaxi.castro@gmail.com>
2020-04-20 15:00:14 -0300

    agregue al TAD Boton la funcion setImage

--- App Learning/src/Boton.java
+++ App Learning/src/Boton.java
@@ -1,3 +1,4 @@
+import javax.swing.ImageIcon;
 import javax.swing.JToggleButton;
 
 public class Boton 
@@ -9,5 +10,12 @@ public class Boton
 	{
 		id = n;
 		boton = new JToggleButton();
+	}
+	
+	public void setImagen(JToggleButton b, String i)
+	{
+		ImageIcon imagen = new ImageIcon(i);
+		b.setIcon(imagen);
+		b.setSelectedIcon(imagen);
 	}
 }
